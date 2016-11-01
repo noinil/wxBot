2226330777bfdae90845d8bd60837ab3df6c23c8
Cheng Tan <noinil@gmail.com>
2016-11-02 00:32:02 +0900

    add poems

--- wechat_bot_ct.py
+++ wechat_bot_ct.py
@@ -3,6 +3,8 @@
 
 import subprocess
 from wxbot import WXBot
+import re
+import random
 
 class MyWXBot(WXBot):
     def __init__(self):
@@ -10,6 +12,10 @@ class MyWXBot(WXBot):
         self.robot_switch = True
         self.away_status = False
         self.ai_status = False
+        self.poem_lines = []
+        with open('/Users/noinil/Learningspace/python_test/tang_poem/tang.txt', 'r') as fin:
+            for line in fin:
+                self.poem_lines.append(line.strip())
 
     def auto_switch(self, msg):
         msg_data = msg['content']['data']
@@ -24,26 +30,33 @@ class MyWXBot(WXBot):
                 if i == msg_data:
                     self.robot_switch = False
                     self.send_msg_by_uid(u'[Bot.v3] ' + u'机器人已关闭！', msg['to_user_id'])
+                    return 0
             if msg_data == away_cmd and self.away_status == False:
                 self.away_status = True
                 self.send_msg_by_uid(u'[Bot.v3] ' + u' Master away from keyboard！', msg['to_user_id'])
+                return 0
             elif msg_data == back_cmd and self.away_status == True:
                 self.away_status = False
                 self.send_msg_by_uid(u'[Bot.v3] ' + u' Master! Welcome back！', msg['to_user_id'])
+                return 0
             elif msg_data == ai_on_cmd and self.ai_status == False:
                 self.ai_status = True
                 self.send_msg_by_uid(u'[Bot.v3] ' + u' AI turned on！', msg['to_user_id'])
+                return 0
             elif msg_data == ai_off_cmd and self.ai_status == True:
                 self.ai_status = False
                 self.send_msg_by_uid(u'[Bot.v3] ' + u' AI turned off！', msg['to_user_id'])
+                return 0
         else:
             for i in start_cmd:
                 if i == msg_data:
                     self.robot_switch = True
                     self.send_msg_by_uid(u'[Bot.v3] ' + u'机器人已开启！', msg['to_user_id'])
+                    return 0
+        return 1
 
     def tell_fortune(self, msg_data):
-        reply = '[Bot.v3] '
+        reply = '[Bot.fortune]: '
         if u'好无聊' in msg_data:
             response = subprocess.Popen(['fortune', '-a'], stdout=subprocess.PIPE).communicate()[0]
         elif u'来首诗歌' in msg_data:
@@ -53,20 +66,63 @@ class MyWXBot(WXBot):
         reply += response
         return reply
 
+    def q_poem(self, msg_data):
+        reply = '[Bot.poet] '
+        search_reg = re.compile(msg_data.encode('utf-8'))
+        for dict_word in self.poem_lines:
+            result = re.search(search_reg, dict_word)
+            if result:
+                m_pos = re.match(search_reg, dict_word)
+                if m_pos:
+                    reply += dict_word[m_pos.end():]
+                else:
+                    reply += dict_word[:result.start()]
+        print(reply)
+        if len(reply) < 12:
+            while True:
+                ss = random.choice(self.poem_lines)
+                if len(ss) > 14:
+                    reply += ss
+                    break
+        return reply
+
+    def q_master_poem(self, msg_data):
+        reply = '[Bot.poet] '
+        search_reg = re.compile(msg_data.encode('utf-8'))
+        for dict_word in self.poem_lines:
+            result = re.search(search_reg, dict_word)
+            if result:
+                reply += dict_word
+                reply += '   '
+        return reply
+
     def handle_msg_all(self, msg):
+        fortune_cmd = [u'好无聊', u'来首诗歌', u'污一个']
         if not self.robot_switch and msg['msg_type_id'] != 1:
             self.send_msg_by_uid(u'[Bot.v3] ' + u'机器人已关闭... 請稍後再聯系！', msg['to_user_id'])
             return
         if msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # reply to self
             master_input = msg["content"]["data"]
-            reply = self.tell_fortune(master_input)
-            self.send_msg_by_uid(reply, msg['to_user_id'])
-            self.auto_switch(msg)
+            if master_input in fortune_cmd:
+                reply = self.tell_fortune(master_input)
+                self.send_msg_by_uid(reply, msg['to_user_id'])
+            elif len(master_input) == 5 or len(master_input) == 7:
+                reply = self.q_poem(master_input)
+                if len(reply) > 12:
+                    self.send_msg_by_uid(reply, msg['to_user_id'])
+            else:
+                istatus = self.auto_switch(msg)
+                
         elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
             if msg['user']['name'] == u'明明':
                 master_input = msg["content"]["data"]
-                reply = self.tell_fortune(master_input)
-                self.send_msg_by_uid(reply, msg['user']['id'])
+                if master_input in fortune_cmd:
+                    reply = self.tell_fortune(master_input)
+                    self.send_msg_by_uid(reply, msg['user']['id'])
+                elif len(master_input) == 5 or len(master_input) == 7:
+                    reply = self.q_poem(master_input)
+                    if len(reply) > 12:
+                        self.send_msg_by_uid(reply, msg['to_user_id'])
             if self.away_status == True:
                 self.send_msg_by_uid(u'[Bot.v3] ' + u' 主人不在, 请稍后联系! ', msg['user']['id'])
                 # TODO Add some functions!
@@ -110,8 +166,13 @@ class MyWXBot(WXBot):
                     if msg['content']['type'] == 0:  # text message
                         user_input = msg["content"]["desc"]
                         src_name = msg['content']['user']['name']
-                        reply = self.tell_fortune(master_input)
-                        self.send_msg_by_uid(reply, msg['user']['id'])
+                        if user_input in fortune_cmd:
+                            reply = self.tell_fortune(user_input)
+                            self.send_msg_by_uid(reply, msg['user']['id'])
+                        elif len(user_input) == 5 or len(user_input) == 7:
+                            reply = self.q_poem(user_input)
+                            if len(reply) > 12:
+                                self.send_msg_by_uid(reply, msg['to_user_id'])
 
         elif msg['msg_type_id'] == 5 and msg['content']['type'] == 0:
             # if msg['user']['name'] == u'小冰':
